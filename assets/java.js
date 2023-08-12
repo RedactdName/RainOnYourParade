606f6d5c426be6e47f152ff49f321f856748098b
Cody <ccoff1798@gmail.com>
2023-08-12 13:57:18 -0600

    cleaned up javascript, workinggit add -Agit add -A

--- assets/java.js
+++ assets/java.js
@@ -62,35 +62,32 @@ searchButton.addEventListener('click', function(event) {
     // console.log(fetchEventsByCity())
     
 })
-function populateEvents(data)
-{
-    const ulEl = document.createElement('ul')
-    const weatherContainer = document.getElementById('weather-container')
-    
-    //for loop
- for (let i = 0; i < data.events.length; i++) {
-    let eventName = data.events[i].title;
-    let liEl = document.createElement('li')
-    liEl.textContent = eventName
-    ulEl.appendChild(liEl)
-        
 
-    weatherContainer.appendChild(ulEl)
- }
-    //get the events name
-    //get the events date
-    //put it on the page in a ul
-    //assign id using name of event
-    //setAttribute("id", eventName + datetime_local) set class to bulma values for style
-    console.log(data)
+//puts events on page
+function populateEvents(data) {
+    const ulEl = document.createElement('ul');
+    const weatherContainer = document.getElementById('weather-container');
+    
+    //still the same forloop Lisa worked on, didn't modify
+    for (let i = 0; i < data.events.length; i++) {
+        let eventName = data.events[i].title;
+        let eventDate = data.events[i].datetime_local.split("T")[0]; // extracting the date from the datetime
+        
+        let liEl = document.createElement('li');
+        liEl.textContent = eventName;
+        ulEl.appendChild(liEl);
+
+        // this is a proof of concept to make sure it works, it is listing out weather for every event
+        //Should NOT be in for loop
+        getCoords(data.events[i].venue.city, eventDate);
+    }
+    weatherContainer.appendChild(ulEl);
+    console.log(data);
 }
 
-//need event listner
-//need that event listener to get ID of item clicked
-//when clicked we get that info from it, 
 
 //gets coordinates from city name
-function getCoords(cityName) {
+function getCoords(cityName, eventDate) {
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${openWeatherAPIKey}`)
         .then(function(response) {
             return response.json();
@@ -98,133 +95,21 @@ function getCoords(cityName) {
         .then(function(data) {
             let lat = data[0].lat;
             let lon = data[0].lon;
-            getTodaysWeather(lat, lon);
-            get5day(lat, lon);
+            //Passing to get weather by date
+            getWeatherByDate(lat, lon, eventDate);
         });
 }
-//gets todays weather
-function getTodaysWeather(lat, lon) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        populateWeather(data);
-    });
-}
 
-//populates weather information
-function populateWeather(data) {
-    let currentTemp = data.main.temp;
-    let humidity = data.main.humidity;
-    let feelsLike = data.main.feels_like;
-    let windspeed = data.wind.speed;
-    let description = data.weather[0].description;
-    let icon = data.weather[0].icon;
-
-    const currentWeatherContainer = document.getElementById('current-weather-container')
-    const pEl = document.createElement('p');
-    const pEl2 = document.createElement('p');
-    const ulEl = document.createElement('ul');
-    const liEl1 = document.createElement('li');
-    const liEl2 = document.createElement('li');
-    const liEl3 = document.createElement('li');
-    const liEl4 = document.createElement('li');
-
-    pEl.textContent = currentTemp;
-    pEl2.textContent = cityName;
-    currentWeatherContainer.appendChild(pEl);
-    currentWeatherContainer.appendChild(pEl2)
-
-    liEl1.textContent = `Humidity: ${humidity}`;
-    ulEl.appendChild(liEl1);
-
-    liEl2.textContent = `Feels Like: ${feelsLike}`;
-    ulEl.appendChild(liEl2);
-
-    liEl3.textContent = `Wind Speed: ${windspeed}`;
-    ulEl.appendChild(liEl3);
-
-    liEl4.textContent = `Description: ${description}`;
-    ulEl.appendChild(liEl4);
-
-    ulEl.setAttribute("id", cityName )
-    
-    currentWeatherContainer.appendChild(ulEl);
-
-    let iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
-    const imgEl = document.createElement('img');
-    imgEl.setAttribute("src", iconUrl);
-    currentWeatherContainer.appendChild(imgEl);
-}
-
-//gets 5 day forcast
-function get5day(lat, lon, eventTime) {
-    let urlByLatLon = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-    
-    fetch(urlByLatLon)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        const currentWeatherContainer = document.getElementById('current-weather-container')
-        currentWeatherContainer.innerHTML = ''; 
-        
-            let forecastItem = data.list[eventTime];
-            let forecastDateTime = new Date(forecastItem.dt * 1000);
-            
-            const forecastItemContainer = document.createElement('div');
-            forecastItemContainer.classList.add('forecast-item');
-            
-            const pDateTime = document.createElement('p');
-            pDateTime.textContent = forecastDateTime.toLocaleString() + ` ${cityName}`
-            forecastItemContainer.appendChild(pDateTime);
-            
-            const ulEl = document.createElement('ul');
-            
-            const liEl1 = document.createElement('li');
-            liEl1.textContent = `Temperature: ${forecastItem.main.temp}`;
-            ulEl.appendChild(liEl1);
-
-            const liEl2 = document.createElement('li');
-            liEl2.textContent = `Humidity: ${forecastItem.main.humidity}`;
-            ulEl.appendChild(liEl2);
-
-            const liEl3 = document.createElement('li');
-            liEl3.textContent = `Feels Like: ${forecastItem.main.feels_like}`;
-            ulEl.appendChild(liEl3);
-
-            const liEl4 = document.createElement('li');
-            liEl4.textContent = `Wind Speed: ${forecastItem.wind.speed}`;
-            ulEl.appendChild(liEl4);
-
-            const liEl5 = document.createElement('li');
-            liEl5.textContent = `Description: ${forecastItem.weather[0].description}`;
-            ulEl.appendChild(liEl5);
-
-            forecastItemContainer.appendChild(ulEl);
-
-            const iconUrl = `https://openweathermap.org/img/wn/${forecastItem.weather[0].icon}@2x.png`;
-            const imgEl = document.createElement('img');
-            imgEl.setAttribute("src", iconUrl);
-            forecastItemContainer.appendChild(imgEl);
-
-            currentWeatherContainer.appendChild(forecastItemContainer);
-        
-    });
-}
+//handles weather info, a lot easier than doing it by date
 function getWeatherByDate(lat, lon, date){
-    // Date in the `YYYY-MM-DD` format for which data is requested. Date available from 1979-01-02 up to the previous day before the current date.
     fetch(`https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=${lat}&lon=${lon}&date=${date}&appid=${openWeatherAPIKey}`)
     .then(function(response){
-       return response.json
+       return response.json();
     }).then(function(data){
-        console.log(data)
-     })
-
+        // weather logging
+        console.log(data);
+     });
 }
 
 
 
-//To get weather info rightreturn date url datetime_local
