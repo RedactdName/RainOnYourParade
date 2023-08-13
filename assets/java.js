1dc844f21eb63d3d59a834037a87d83226a1c4a8
Cody <ccoff1798@gmail.com>
2023-08-12 18:05:51 -0600

    Oh boy its working, weather in console log. Added comments everywhere

--- assets/java.js
+++ assets/java.js
@@ -1,115 +1,146 @@
 //Variables required for seatGeek
 const clientId = 'client_id=MzU1MTEzMTF8MTY5MTQ1ODY0Mi45OTk0NA'
 const clientSecret = 'client_secret=8eab9cae7143c435b6897a8ee23b044cc8152e61df06a4568d536acb71b31a12`'
-// const seatGeekSearchByCityURL = `https://api.seatgeek.com/2/events?venue.city=${city}&${clientId}&per_page=${perPage}&page=${page}&datetime_utc.gte=${startDate}&datetime_utc.lte=${endDate}`
-// const seatGeek = `https://api.seatgeek.com/2/events?${clientId}&${clientSecret}`
 
 //Variables required for Open Weather
 const openWeatherAPIKey = '853c7e45dec1706d5f746a46f8d87cf8'
-let cityName = ''
-let lat = 0
-let lon = 0
-let weatherByCoords = 'https://api.openweathermap.org/data/2.5/forecast?lat='+ lat +'&lon='+ lon +'&appid='+ openWeatherAPIKey + '&units=imperial'
-let weatherByCity = 'https://api.openweathermap.org/data/2.5/forecast?q='+ cityName +'&appid='+ openWeatherAPIKey
 
-//variables to grab //recents changed
+//variables to grab
 const startDateContainer = document.getElementById("start-date")
 const endDateContainer = document.getElementById("end-date")
 const cityContainer = document.getElementById("city")
 const searchButton = document.getElementById("search-button")
 
 //function to initiate fetch by city
-function fetchEventsByCity(){
+function fetchEventsByCity() {
+    //getting values from containers
     let city = cityContainer.value
     let perPage = 30
     let page = 1
     let startDate = startDateContainer.value
-    let endDate = endDateContainer.value 
-    if(city == ''){
+    let endDate = endDateContainer.value
+    //setting null check
+    if (city == '') {
         city = 'Denver'
     }
-    if(startDate == '')
-    {
+    if (startDate == '') {
         startDate = '08-10-2023'
     }
-    if(endDate == '')
-    {
+    if (endDate == '') {
         endDate = '08-30-2023'
     }
-
-fetch(`https://api.seatgeek.com/2/events?venue.city=${city}&${clientId}&per_page=${perPage}&page=${page}&datetime_utc.gte=${startDate}&datetime_utc.lte=${endDate}`)
-.then(function(response){
-   return response.json()
-})
-.then(function(data){
-    // console.log(data)
-    populateEvents(data)
-})
+    //fetching the events listing
+    fetch(`https://api.seatgeek.com/2/events?venue.city=${city}&${clientId}&per_page=${perPage}&page=${page}&datetime_utc.gte=${startDate}&datetime_utc.lte=${endDate}`)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            // calling populate events function with the jsoned data
+            populateEvents(data)
+        })
 }
 
-// fetchEventsByCity(cityName) //recently changed
-searchButton.addEventListener('click', function(event) {
+// the event listner for the search button
+searchButton.addEventListener('click', function (event) {
     event.preventDefault()
     console.log('clicked')
-    event.stopPropagation()//not right
-    startDate = startDateContainer.value //all these are new
+    startDate = startDateContainer.value
     endDate = endDateContainer.value
     city = cityContainer.value
 
     fetchEventsByCity()
-
-
-    // console.log(fetchEventsByCity())
-    
 })
 
 //puts events on page
 function populateEvents(data) {
     const ulEl = document.createElement('ul');
     const weatherContainer = document.getElementById('weather-container');
-    
-    //still the same forloop Lisa worked on, didn't modify
+
+    //getting all needed info from the events
     for (let i = 0; i < data.events.length; i++) {
+        //setting name and time
         let eventName = data.events[i].title;
-        let eventDate = data.events[i].datetime_local.split("T")[0]; // extracting the date from the datetime
-        
+        let eventDateTime = data.events[i].datetime_local
+
+        //creating the list item and adding text content
         let liEl = document.createElement('li');
         liEl.textContent = eventName;
+        //adding the list item to the unordered list, we may change this later to be a section of its own instead of list item
         ulEl.appendChild(liEl);
 
-        // this is a proof of concept to make sure it works, it is listing out weather for every event
-        //Should NOT be in for loop
-        getCoords(data.events[i].venue.city, eventDate);
+        // adding event listner to each of the li's.
+        liEl.addEventListener('click', function () {
+            //calling getcoords function for each event potentially we could simplify to only city, or expand to exact venue but IDK how to do that
+            return getCoords(data.events[i].venue.city, eventDateTime)
+        })
+        //just throwing it in the console to make sure it works
+        console.log(data.events[i].datetime_local);
     }
+    //appending the unoredered list to the weather container, need britanny to help me make it look good
     weatherContainer.appendChild(ulEl);
-    console.log(data);
 }
-
-
 //gets coordinates from city name
-function getCoords(cityName, eventDate) {
+function getCoords(cityName, eventDateTime) {
+    //fetching the coords, passing eventDateTime to push through in a seperate function
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${openWeatherAPIKey}`)
-        .then(function(response) {
+        .then(function (response) {
             return response.json();
         })
-        .then(function(data) {
+        .then(function (data) {
+            //only calling most popular option, maybe play with later to get multiple city options
             let lat = data[0].lat;
             let lon = data[0].lon;
-            //Passing to get weather by date
-            getWeatherByDate(lat, lon, eventDate);
+            //calling function now that I am getting the data I need
+            getWeatherByDate(lat, lon, eventDateTime);
         });
 }
+// Updated function to get weather by date and time
+function getWeatherByDate(lat, lon, eventDateTime) {
+    //fetching by coords
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,daily,alerts&appid=${openWeatherAPIKey}&units=imperial`)
+        .then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            // getting hours from event by calling function
+            const eventHour = extractHours(eventDateTime);
 
-//handles weather info, a lot easier than doing it by date
-function getWeatherByDate(lat, lon, date){
-    fetch(`https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=${lat}&lon=${lon}&date=${date}&appid=${openWeatherAPIKey}`)
-    .then(function(response){
-       return response.json();
-    }).then(function(data){
-        // weather logging
-        console.log(data);
-     });
+            // setting function to null so it doesnt store data
+            let weatherForEventHour = null;
+
+            // Loop through each hour's data to find a match
+            for (let hourData of data.hourly) {
+
+                // formats the time right
+                const hourDataTime = new Date(hourData.dt * 1000);
+
+                // if it matches the events hour
+                if (hourDataTime.getHours() === eventHour) {
+                    //save weather data
+                    weatherForEventHour = hourData;
+                }
+            }
+            //logging what we got
+            if (weatherForEventHour) {
+                console.log(weatherForEventHour);
+            } else {
+                //if there isnt any data
+                console.log("Couldn't find weather data for the specified event hour.");
+            }
+        });
 }
+//getting hours out of the date
+function extractHours(dateTime) {
+    //gets time passed to it and sets time
+    const dt = new Date(dateTime);
+
+    //returning the hours of the dt
+    return dt.getHours();
+}
+
+
+
+
+
 
 
 
